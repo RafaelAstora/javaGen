ba055e5a2188a5b8da14ec3a009684aa28ae7375
RafaelAstora <rafad2106@gmail.com>
2023-02-08 15:23:07 -0300

    correcao do int para float

--- Aula_01/src/aula_01/atvd4.java
+++ Aula_01/src/aula_01/atvd4.java
@@ -7,16 +7,16 @@ public class atvd4 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int n1,n2,n3,n4,diferenca;
+		Float n1,n2,n3,n4,diferenca;
 		
 		System.out.println("Insira o numero 1:");
-		n1 = sc.nextInt();
+		n1 = sc.nextFloat();
 		System.out.println("Insira o numero 2:");
-		n2 = sc.nextInt();
+		n2 = sc.nextFloat();
 		System.out.println("Insira o numero 3:");
-		n3 = sc.nextInt();
-		System.out.println("Insira o numero 4:");
-		n4 = sc.nextInt();
+		n3 = sc.nextFloat();
+		System.out.print("Insira o numero 4:");
+		n4 = sc.nextFloat();
 		
 		diferenca = (n1*n2)-(n3*n4);
 		
